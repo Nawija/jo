52da9a7acd3c380622593fd34377e9912d95ffea
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-01 15:22:52 +0100

    df

---
+++ ui/background/Background.tsx
@@ -0,0 +1,8 @@
+"use client";
+export function Background() {
+    return (
+        <div className="h-screen w-full absolute top-0 left-0 bg-green-400">
+            <p>xxxx</p>
+        </div>
+    );
+}
